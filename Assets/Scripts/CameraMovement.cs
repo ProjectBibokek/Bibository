b86c8dbf747d8c56d5595687980669a30de0ad33
MaddockMelpo <madokamelpo@gmail.com>
2017-02-24 18:25:41 +0300

    Added prefab scene and basic player raycast movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+    public GameObject playerModel;
+    private Vector3 offset;
+    void Start () {
+        offset = transform.position - playerModel.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        transform.position = playerModel.transform.position + offset;
+    }
+
+    public void focusOnPlayerModel() //Перемещает камеру на позицию модели персонажа
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Ray movementRay;
+    NavMeshAgent navMeshAgent;
+
+	// Use this for initialization
+	void Start () {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit mousePointer;
+        movementRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Input.GetMouseButtonUp(1))
+        {
+            if (Physics.Raycast(movementRay, out mousePointer, 100.0f))
+            {
+                navMeshAgent.SetDestination(mousePointer.point);
+            }
+        }
+
+	}
+}
